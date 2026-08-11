569b800d5907232652104ecc13ace31626263c07
MarcosRios7 <marcosrios246@gmail.com>
2026-08-10 20:22:41 -0600

    Cambios de nuevo en el manifest y en el index correccion de la sintaxis para sacar la informacion de las fotos

--- sw.js
+++ sw.js
@@ -1,7 +1,26 @@
+const CACHE_NAME = 'sw-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/materialize.min.css',
+  './css/styles.css',
+  './js/materialize.min.js',
+  './manifest.json'
+];
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('sw-cache').then(function(cache) {
-            return cache.add('index.html');
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// El evento fetch es OBLIGATORIO para que el navegador considere instalable la PWA
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
         })
     );
 });
